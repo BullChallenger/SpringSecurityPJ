b9b6b3dc17b5e7365bf14012bd42d5cf5b607545
Seungbin <am23am45@naver.com>
2022-12-09 23:22:58 +0900

    Add Logout Filter with Handler

--- basicSecurity/src/main/java/io/security/basicSecurity/config/SecurityConfig.java
+++ basicSecurity/src/main/java/io/security/basicSecurity/config/SecurityConfig.java
@@ -8,10 +8,13 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -33,6 +36,14 @@ public class SecurityConfig {
                 .successHandler(authenticationSuccessHandler())
                 .failureHandler(authenticationFailureHandler())
                 .permitAll();
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(logoutHandler())
+                .logoutSuccessHandler(logoutSuccessHandler())
+                .deleteCookies("remember-me");
 
         return http.build();
     }
@@ -56,5 +67,26 @@ public class SecurityConfig {
                 response.sendRedirect("/login");
             }
         };
+    }
+
+    @Bean
+    public LogoutHandler logoutHandler() {
+        return new LogoutHandler() {
+            @Override
+            public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                HttpSession httpSession = request.getSession();
+                httpSession.invalidate();
+            }
+        };
+    }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        return new LogoutSuccessHandler() {
+            @Override
+            public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                response.sendRedirect("/login");
+            }
+        }
     }
 }
